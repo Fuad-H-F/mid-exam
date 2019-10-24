2bbaa0fae7a5621f232aaccf63bf317b23e7ed55
Fuad-H-F <53287594+Fuad-H-F@users.noreply.github.com>
2019-10-24 13:35:54 +0600

    Add files via upload

--- mid exam/controllers/admin.js
+++ mid exam/controllers/admin.js
@@ -1,11 +1,33 @@
 var express = require('express');
-var db = require('./../models/db.js');
+var regModel = require('./../models/registration-model');
 var router = express.Router();
 
 router.get('/', function(req, res){
 
 	res.render('admin/index');
 			
+});
+router.get('/admo', function(req, res){
+
+	res.render('admin/admo');
+			
+});
+router.post('/admo', function(req, res){
+	var user = {
+		fullname: req.body.fullname,
+		username: req.body.username,
+		password: req.body.password,
+		type	: "moderator"
+	}
+
+	regModel.insert(user, function(status){
+		if(status){
+			console.log("successful");
+			res.redirect('/admin');
+		}else{
+			res.redirect('/admo');
+		}
+	});
 });
 
 router.get('/test/:name/:id', function(req, res){
